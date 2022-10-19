29cfdadcedeacff362e5f0b49b72632aec81d1ae
Giosué Russo <josh@hizen.io>
2022-10-19 19:32:20 +0900

    sampla pace update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
 
-  let url = "https://www.youtube.com/watch?v=dBwyVtFCNfc&ab_channel=Dave2D"
+  let counter = 0
+  let toggleOn = false
 
-  let counter = 1
-
+  function toggle(){
+    counter ++
+    toggleOn = !toggleOn
+  }
 </script>
 
 <div class="flex grow flex-col">
 
-  <div class="flex p-4 bg-accent/20">
-    <input type="text" class="w-full input" value="{url}">
-  </div>
-
   <div class="justify-center items-center grow flex p-4 flex-col gap-4">
     <!-- <iframe title="" src="{url}" frameborder="0" class="w-full h-full"></iframe> -->
     <div>{counter}</div>
-    <button class="btn btn-primary" on:click={ ()=> counter ++ }>click</button>
+    <button class:btn-accent={toggleOn} class="btn btn-primary" on:click={ ()=> toggle() }>click</button>
   </div>
-
-
 
 </div>
